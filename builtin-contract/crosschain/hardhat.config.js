f66e494f7c6caa6e5995af1ada3170477581e81f
daryl <sai9375@hotmail.com>
2022-07-01 14:39:36 +0800

    feat(crosschain): add some tools to send tx to axon contract.

--- builtin-contract/crosschain/hardhat.config.js
+++ builtin-contract/crosschain/hardhat.config.js
@@ -1,8 +1,7 @@
 const { task } = require("hardhat/config");
 
 require("@nomiclabs/hardhat-waffle");
-// const {abi} = require('./artifacts/contracts/crosschain.sol/CrossChain.json');
-require('fs');
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,12 +16,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 task('deployTestToken', 'deploy test token on axon').addParam('private').setAction(async (taskArgs, hre) => {
   const signer = new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
   const TestToken = await hre.ethers.getContractFactory('TestToken');
-  let unsignedTx = await TestToken.getDeployTransaction();
+  let unsignedTx = TestToken.getDeployTransaction();
   unsignedTx = await signer.populateTransaction(unsignedTx);
   unsignedTx.nonce = await signer.getTransactionCount() + 1;
   const signedTx = await signer.signTransaction(unsignedTx);
   const tx = await hre.ethers.provider.sendTransaction(signedTx);
-  console.log(tx.hash);
+  const recepient = await tx.wait();
+  console.log(recepient);
+});
+
+task('deployMirrorToken', 'deploy mirror token on axon').addParam('private').addParam('name').addParam('symbol').setAction(async (taskArgs, hre) => {
+  const signer = new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
+  const MirrorToken = await hre.ethers.getContractFactory('MirrorToken');
+  let unsignedTx = MirrorToken.getDeployTransaction(taskArgs.name, taskArgs.symbol);
+  unsignedTx = await signer.populateTransaction(unsignedTx);
+  unsignedTx.nonce = await signer.getTransactionCount() + 1;
+  const signedTx = await signer.signTransaction(unsignedTx);
+  const tx = await hre.ethers.provider.sendTransaction(signedTx);
+  const recepient = await tx.wait();
+  console.log(recepient);
 });
 
 task('crossAt', 'cross at').addParam('to').addParam('private').setAction(async (taskArgs, hre) => {
@@ -30,14 +42,15 @@ task('crossAt', 'cross at').addParam('to').addParam('private').setAction(async (
   const signer =new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
   let unsignedTx = {
     data: abi.encodeFunctionData('lockAT', [taskArgs.to]),
-    to: '0xF67Bc4E50d1df92b0E4C61794A4517AF6a995CB2',
+    to: hre.ethers.utils.getAddress('0xF67Bc4E50d1df92b0E4C61794A4517AF6a995CB2'),
     value: hre.ethers.utils.parseEther('0.1'),
   };
   unsignedTx = await signer.populateTransaction(unsignedTx);
   unsignedTx.nonce = await signer.getTransactionCount() + 1;
   const signedTx = await signer.signTransaction(unsignedTx);
   const tx = await hre.ethers.provider.sendTransaction(signedTx);
-  console.log(tx.hash);
+  const recepient = await tx.wait();
+  console.log(recepient);
 });
 
 task('crossToken', 'cross token').addParam('to').addParam('token').addParam('amount').addParam('private').setAction(async (taskArgs, hre) => {
@@ -54,6 +67,63 @@ task('crossToken', 'cross token').addParam('to').addParam('token').addParam('amo
   console.log(tx.hash);
 });
 
+task('grantWCKBRole').addParam('private').addParam('to').setAction(async (taskArgs, hre) => {
+  const abi = new hre.ethers.utils.Interface(require('./artifacts/contracts/MirrorToken.sol/MirrorToken.json').abi);
+  const signer =new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
+  let unsignedTx = {
+    data: abi.encodeFunctionData('grantRole', [hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes('MANAGER_ROLE')), taskArgs.to]),
+    to: hre.ethers.utils.getAddress('0x4af5ec5e3d29d9ddd7f4bf91a022131c41b72352'),
+  };
+  unsignedTx = await signer.populateTransaction(unsignedTx);
+  unsignedTx.nonce = await signer.getTransactionCount() + 1;
+  const signedTx = await signer.signTransaction(unsignedTx);
+  const tx = await hre.ethers.provider.sendTransaction(signedTx);
+  const recepient = await tx.wait();
+  console.log(recepient);
+});
+
+task('mintCKB').addParam('private').addParam('amount').addParam('to').setAction(async (taskArgs, hre) => {
+  const abi = new hre.ethers.utils.Interface(require('./artifacts/contracts/MirrorToken.sol/MirrorToken.json').abi);
+  const signer =new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
+  console.log(signer.address);
+  let unsignedTx = {
+    data: abi.encodeFunctionData('mint', [taskArgs.to, hre.ethers.utils.parseUnits(taskArgs.amount, 18)]),
+    to: hre.ethers.utils.getAddress('0x4af5ec5e3d29d9ddd7f4bf91a022131c41b72352'),
+  };
+  unsignedTx = await signer.populateTransaction(unsignedTx);
+  unsignedTx.nonce = await signer.getTransactionCount() + 1;
+  const signedTx = await signer.signTransaction(unsignedTx);
+  const tx = await hre.ethers.provider.sendTransaction(signedTx);
+  const recepient = await tx.wait();
+  console.log(recepient);
+});
+
+task('wckbBalance').addParam('account').setAction(async (taskArgs, hre) => {
+  const abi = new hre.ethers.utils.Interface(require('./artifacts/contracts/MirrorToken.sol/MirrorToken.json').abi);
+  const signer = await hre.ethers.getSigner();
+  const contract = new hre.ethers.Contract('0x4af5ec5e3d29d9ddd7f4bf91a022131c41b72352', abi, signer);
+  console.log(await contract.balanceOf(taskArgs.account));
+});
+
+task('setTokenConfig').addParam('token').addParam('threshold').addParam('fee').addParam('private').setAction(async (taskArgs, hre) => {
+  const abi = new hre.ethers.utils.Interface(require('./artifacts/contracts/crosschain.sol/CrossChain.json').abi);
+  const signer =new hre.ethers.Wallet(taskArgs.private, hre.ethers.provider);
+  let unsignedTx = {
+    data: abi.encodeFunctionData('setTokenConfig', [taskArgs.token, [hre.ethers.BigNumber.from(taskArgs.threshold), hre.ethers.BigNumber.from(taskArgs.fee)]]),
+    to: hre.ethers.utils.getAddress('0xF67Bc4E50d1df92b0E4C61794A4517AF6a995CB2'),
+  };
+  unsignedTx = await signer.populateTransaction(unsignedTx);
+  unsignedTx.nonce = await signer.getTransactionCount() + 1;
+  const signedTx = await signer.signTransaction(unsignedTx);
+  const tx = await hre.ethers.provider.sendTransaction(signedTx);
+  const recepient = await tx.wait();
+  console.log(recepient);
+});
+
+task('getTransaction').addParam('tx').setAction(async (taskArgs, hre) => {
+  console.log(await hre.ethers.provider.getTransaction(taskArgs.tx));
+});
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -64,8 +134,9 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     axon: {
-      url: 'http://13.113.146.156:8000',
-      accounts: [],
+      url: process.env.AXON_URL|| "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
